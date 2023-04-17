51db345e603f963c9ef7782cd72da9c3d3f78950
mad rider <mad-ride.rider@yandex.ru>
2023-04-17 23:23:21 +0300

    homeWork task_23 done

--- 17.04_task23/Program.cs
+++ 17.04_task23/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+
+void TableAllSquares(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 3}  |{Math.Pow(index, 3), 5}");
+        index++;
+    }
+}
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableAllSquares(number);
