19f52c20d6327e9e64b4f24721bdd69a6c2167af
mad rider <mad-ride.rider@yandex.ru>
2023-04-17 21:41:57 +0300

    seminar 3 all files

---
+++ 14.04_task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+Console.WriteLine("Введите номер четверти");
+int numberforth = Convert.ToInt32(Console.ReadLine());
+
+
+string quarter = Quarter(numberforth);
+Console.WriteLine(quarter);
+
+string Quarter(int x)
+{
+    if(x == 1) return "x > 0 and y > 0";
+    if(x == 2) return "x < 0 and y > 0";
+    if(x == 3) return "x < 0 and y < 0";
+    if(x == 4) return "x > 0 and y < 0";
+    return "error";
+}
---
+++ 14.04_task22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+
+void TableAllSquares(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 3}  {index * index, 5}");
+        index++;
+    }
+}
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableAllSquares(number);
