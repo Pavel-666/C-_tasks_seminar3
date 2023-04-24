86896cf5ea70a35c2a1ad186f9e168a1edc20935
mad rider <mad-ride.rider@yandex.ru>
2023-04-25 01:32:17 +0300

    task 34 done

---
+++ 24.04_task34/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArray (int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+else Console.Write($"{arr[i]}");
+}
+}
+
+int SearchHonestElements (int[] arr)
+{
+int temp = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+if(arr[i] % 2 == 0) temp += 1;
+}
+return temp;
+}
+
+Console.WriteLine("укажите размер массива");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int minArray = 100;
+int maxArray = 999;
+int[] array = CreateArrayRndInt(sizeArray, minArray, maxArray);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+int honest = SearchHonestElements(array);
+Console.WriteLine($"количество четных элементов = {honest}");
