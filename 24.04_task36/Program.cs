78c2d42340419372f518e77c3d67390202c8aca0
mad rider <mad-ride.rider@yandex.ru>
2023-04-25 01:32:33 +0300

    task 36 done

---
+++ 24.04_task36/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArray (int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+else Console.Write($"{arr[i]}");
+}
+}
+
+int SumUnevenElements (int[] arr)
+{
+int sum = 0;
+for (int i = 1; i < arr.Length; i = i + 2)
+{
+    sum += arr[i];
+}
+return sum;
+}
+
+Console.WriteLine("укажите размер массива");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int minArray = 0;
+int maxArray = 99;
+int[] array = CreateArrayRndInt(sizeArray, minArray, maxArray);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+int sumUnevenElements = SumUnevenElements(array);
+Console.WriteLine($"сумма элементов с нечетной позицией = {sumUnevenElements}");
