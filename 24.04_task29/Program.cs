dbcfffba33467665b859ef664db8d1c2d3012275
mad rider <mad-ride.rider@yandex.ru>
2023-04-24 23:13:40 +0300

    task 29 done

---
+++ 24.04_task29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] array = new int[8];
+ArrayIn(array);
+ArrayPrint(array);
+
+void ArrayIn(int[] array)
+{
+   int length = array.Length;
+   for (int i = 0; i <length; i++)
+   {
+      array[i] = new Random().Next(0, 100);
+   }
+}
+
+void ArrayPrint(int[] array)
+{
+   int length = array.Length;
+   Console.Write ("[");
+   for (int i = 0; i < length -1; i++)
+   {
+      
+      Console.Write($"{array[i]}, ");
+   }
+   Console.Write(array[length -1]);
+   Console.Write("]");
+}
