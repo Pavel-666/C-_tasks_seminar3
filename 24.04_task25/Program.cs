18b62049a43a1c70f6b4d678b99e9fec6e446e88
mad rider <mad-ride.rider@yandex.ru>
2023-04-24 23:13:13 +0300

    task 25 done

---
+++ 24.04_task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int Еxponentiation (int numA, int numB)
+{
+   int count = 0;
+   int deg = 1;
+   while (count < numB)
+   {
+        deg = deg * numA;
+        count++;
+   }
+   return deg;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int degree = Convert.ToInt32(Console.ReadLine());
+int exponentiation = Еxponentiation(number, degree);
+Console.WriteLine(exponentiation);
