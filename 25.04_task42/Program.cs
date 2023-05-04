8864abb2dae7c966eecc9cf0858345cd4b038c6a
mad rider <mad-ride.rider@yandex.ru>
2023-05-04 23:54:55 +0300

    seminar done

---
+++ 25.04_task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ConvertTenToTwo (int num)
+{
+   int result = 0;
+   int i = 1;
+   while (num>0)
+   {
+      result+=(num % 2)*i;
+      num /= 2;
+      i*=10;
+   }
+   return result;
+}
+int convertTentoTwo = ConvertTenToTwo(number);
+Console.WriteLine(convertTentoTwo);
---
+++ 28.04_task49/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    //                        0       1
+    int[,] matrix = new int[rows, columns]; // 3, 4
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) //matrix.GetLength(0) = 3
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void ModificMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i+=2) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2) 
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
+ModificMatrix(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
