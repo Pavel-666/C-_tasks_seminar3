cb2c45779ca8a1605ff77c1ee7960ced7489669d
mad rider <mad-ride.rider@yandex.ru>
2023-05-29 22:51:02 +0300

    tasks homework done

---
+++ 29.05_task66/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите чилое натуральное число M");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чилое натуральное число N");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+SumPrint(number, number1);
+
+
+int NaturalNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        int temp = num2;
+        num2++;
+        temp = num2 + NaturalNumbers(num1, num2);
+        return temp;
+    }
+    else if (num1 < num2)
+    {
+        int temp = num1;
+        num1++;
+        temp = num1 + NaturalNumbers(num1, num2);
+        return temp;
+    }
+    else return 0;
+}
+
+
+void SumPrint(int num1, int num2)
+{
+    if (num1 < num2)
+    Console.Write(NaturalNumbers(num1 - 1, num2));
+    else Console.Write(NaturalNumbers(num1, num2 - 1 ));
+}
---
+++ 29.05_task68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.WriteLine("Введите чилое натуральное число M");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чилое натуральное число N");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int akkerman = Akkerman(number, number1);  
+    Console.Write(akkerman); 
+
+int Akkerman(int num1, int num2)
+{
+    if (num1 == 0)
+    {
+        return num2 + 1;
+    }
+    else if (num2 == 0 && num1 > 0)
+    {
+        return Akkerman(num1 - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(num1 - 1, Akkerman(num1, num2 - 1)));
+    }
+}
